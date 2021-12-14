580c5bbd1c866f37725071b58c35b4b800c2b5fd
Harold Gallego <90746562+toor-at-har0@users.noreply.github.com>
2021-12-14 10:00:34 +0800

    modified contactus/js.contactus style

--- js/validate.js
+++ js/validate.js
@@ -4,33 +4,53 @@ form.addEventListener("submit", (e) => {
     let fname = document.getElementById("first-name");
     let lname = document.getElementById("last-name");
     let email = document.getElementById("email-addr");
-    const res_char = [,]
+    var pholder_fname_val = document.getElementById("first-name").placeholder;
+    var pholder_lname_val = document.getElementById("last-name").placeholder;
+    var pholder_email_val = document.getElementById("email-addr").placeholder;
 
         if (fname.value == "" || fname.value.includes('<','>','&','!')) {
-            add_after(fname);
             e.preventDefault();
+            console.log(pholder_fname_val, 'preventDefault() executed')
+            add_pholder_val("phold_fname", pholder_fname_val);
+            add_after(fname);
+            
         }
 
         if (lname.value == "" || fname.value.includes('<','>','&','!')) {
-            add_after(lname);
             e.preventDefault();
+            console.log(pholder_lname_val, 'preventDefault() executed')
+            add_pholder_val("phold_lname", pholder_lname_val);
+            add_after(lname);
+            
+            
         }
         if (email.value == "" || fname.value.includes('<','>','&','!')) {
-            add_after(email);
             e.preventDefault();
+            console.log(pholder_email_val, 'preventDefault() executed')
+            add_pholder_val("phold_email", pholder_email_val);
+            add_after(email);
+            
+            
     }
     return false;
 });
 
-function add_after(varbl) {
-    let x = varbl;
+function add_after(element) {
+    let x = element;
     x.previousElementSibling.classList.add("required");
-    x.style.border = "2px solid red";
+    x.style.border = "1px solid red";
     x.style.background = "#ffdddd";
 }
 
-function mod_border_bg(varbl) {
-    let y = varbl;
+function mod_border_bg(element) {
+    let y = element;
     x.style.border = "1px solid #ccc";
     x.style.background = "#fff";
 }
+
+function add_pholder_val(element, value) {
+    // let z = "" + element;
+    document.getElementById(element).innerHTML = value;
+}
+
+
