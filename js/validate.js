6630ab95f7a2e7e6f52632660078349eb4c16e44
Harold Gallego <90746562+toor-at-har0@users.noreply.github.com>
2021-12-15 11:57:29 +0800

    done JS validation

--- js/validate.js
+++ js/validate.js
@@ -5,17 +5,18 @@ form.addEventListener("submit", (e) => {
     let lname = document.getElementById("last-name");
     let email = document.getElementById("email-addr");
     let q_box = document.getElementById("qstn-box");
+    let q_box_cont = document.getElementById("question-box");
     var pholder_fname_val = document.getElementById("first-name").placeholder;
     var pholder_lname_val = document.getElementById("last-name").placeholder;
     var pholder_email_val = document.getElementById("email-addr").placeholder;
     var pholder_qbox_val = document.getElementById("qstn-box").placeholder;
 
         if (fname.value == "" || fname.value.includes('<','>','&','!')) {
+            
             e.preventDefault(); //prevents submission
             // console.log(pholder_fname_val, 'preventDefault() executed')
             add_pholder_val("phold_fname", pholder_fname_val);
             add_after(fname);
-            
         }
 
         if (lname.value == "" || lname.value.includes('<','>','&','!')) {
@@ -30,14 +31,23 @@ form.addEventListener("submit", (e) => {
             // console.log(pholder_email_val, 'preventDefault() executed')
             add_pholder_val("phold_email", pholder_email_val);
             add_after(email);  
-    }
+        }
         if (q_box.value == "" || q_box.value.includes('<','>','&','!')) {
             e.preventDefault();
             // console.log(pholder_lname_val, 'preventDefault() executed')
             add_pholder_val("phold_qbox", pholder_qbox_val);
             add_after(q_box);
-            q_box.style.marginBottom = "10px";
-    }
+            
+        }
+
+        if (fname.value != "" && lname.value != "" && email.value != "" && q_box.value != "") {
+            document.getElementById("id-txt-container").style.display = "none";
+            document.getElementById("contact-us-container").style.display = "none";
+            document.getElementById("submit-success").style.display = "block";
+            document.getElementById("active-email").innerHTML = email.value;
+            e.preventDefault();
+        }
+        q_box_cont.style.marginBottom = "30px";
     return false;
 });
 
@@ -46,6 +56,11 @@ function add_after(element) {
     x.previousElementSibling.classList.add("required");
     x.style.border = "1px solid red";
     x.style.background = "#ffdddd";
+}
+
+function disable_inps(element) {
+    let inp = document.getElementById(element);
+    document.getElementById(element).disabled = true;
 }
 
 
